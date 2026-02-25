35a887ee2b46680ebc48412e97da6624b74a2bec
dipanshu180 <dipanshu180503@gmail.com>
2026-02-25 23:24:48 +0530

    auth

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,58 +1,173 @@
-from fastapi import APIRouter , Depends, HTTPException , status
-from .schemas import UserCreateModel , UserModel , UserLoginModel
-from sqlalchemy.ext.asyncio import AsyncSession
-from src.db.main import get_session
-from .service import UserService 
-from .utils import generate_password_hash , verify_password , create_access_token
+from datetime import datetime, timedelta
+
+from fastapi import APIRouter, Depends, status, BackgroundTasks
+from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
-from datetime import timedelta , datetime
-from .dependencies import RefreshTokenBearer, AccessTokenBearer
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.mail import create_message, mail
+from src.db.main import get_session
 from src.db.redis import add_jti_to_blocklist
+from src.config import Config
+
+from .dependencies import (
+    AccessTokenBearer,
+    RefreshTokenBearer,
+    RoleChecker,
+    get_current_user,
+)
+from .schemas import (
+    UserBooksModel,
+    UserCreateModel,
+    UserLoginModel,
+    EmailModel,
+    PasswordResetRequestModel,
+    PasswordResetConfirmModel,
+)
+from .service import UserService
+from .utils import (
+    create_access_token,
+    verify_password,
+    generate_passwd_hash, 
+    create_url_safe_token, 
+    decode_url_safe_token, 
+)
+from src.errors import UserAlreadyExists, UserNotFound, InvalidCredentials, InvalidToken
+from src.config import Config
+from src.celery_tasks import send_email
+
 auth_router = APIRouter()
 user_service = UserService()
-REFERESH_TOKEN_EXPIRE = 2
+role_checker = RoleChecker(["admin", "user"])
 
-@auth_router.post("/signup", response_model=UserModel , status_code=status.HTTP_201_CREATED)
-async def create_user_account(user: UserCreateModel, session: AsyncSession = Depends(get_session)):
-    user_exists = await user_service.user_exists(user.email, session)
+
+REFRESH_TOKEN_EXPIRY = 2
+
+
+# Bearer Token
+
+
+@auth_router.post("/send_mail")
+async def send_mail(emails: EmailModel):
+    emails = emails.addresses
+
+    html = "<h1>Welcome to the app</h1>"
+    subject = "Welcome to our app"
+    # message = create_message(emails, subject, html)
+
+    # await mail.send_message(message)
+
+    send_email.delay(emails, subject, html)
+
+    return {"message": "Email sent successfully"}
+
+
+@auth_router.post("/signup", status_code=status.HTTP_201_CREATED)
+async def create_user_Account(
+    user_data: UserCreateModel,
+    bg_tasks: BackgroundTasks,
+    session: AsyncSession = Depends(get_session),
+):
+    print("Creating user account")
+    """
+    Create user account using email, username, first_name, last_name
+    params:
+        user_data: UserCreateModel
+    """
+    email = user_data.email
+
+    user_exists = await user_service.user_exists(email, session)
+
     if user_exists:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User already exists")
+        raise UserAlreadyExists()
+
+    new_user = await user_service.create_user(user_data, session)
     
-    new_user =  await user_service.create_user(user , session)
-    return new_user
+    token = create_url_safe_token({"email": email})
+
+    link = f"http://{Config.DOMAIN}/api/v1/auth/verify/{token}"
+
+    html = f"""
+    <h1>Verify your Email</h1>
+    <p>Please click this <a href="{link}">link</a> to verify your email</p>
+    """
+
+    emails = [email]
+
+    subject = "Verify Your email"
+    # await mail.send_message(create_message(emails, subject, html))
+    send_email.delay(emails, subject, html)
+
+    return {
+        "message": "Account Created! Check email to verify your account",
+        "user": new_user,
+    }
+
+
+@auth_router.get("/verify/{token}")
+async def verify_user_account(token: str, session: AsyncSession = Depends(get_session)):
+
+    token_data = decode_url_safe_token(token)
+
+    user_email = token_data.get("email")
+
+    if user_email:
+        user = await user_service.get_user_by_email(user_email, session)
+
+        if not user:
+            raise UserNotFound()
+
+        await user_service.update_user(user, {"is_verified": True}, session)
+
+        return JSONResponse(
+            content={"message": "Account verified successfully"},
+            status_code=status.HTTP_200_OK,
+        )
+
+    return JSONResponse(
+        content={"message": "Error occured during verification"},
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
+
 
 @auth_router.post("/login")
-async def login(login_data : UserLoginModel, session:AsyncSession = Depends(get_session)):
+async def login_users(
+    login_data: UserLoginModel, session: AsyncSession = Depends(get_session)
+):
     email = login_data.email
     password = login_data.password
 
     user = await user_service.get_user_by_email(email, session)
 
     if user is not None:
-        
         password_valid = verify_password(password, user.password_hash)
-        if password_valid:
-            access_token = create_access_token(user_data={"uid": str(user.uid), "email": user.email})
 
-            referesh_token = create_access_token(user_data={"uid": str(user.uid), "email": user.email}, refresh=True,
-            expiry=timedelta(days=REFERESH_TOKEN_EXPIRE)
+        if password_valid:
+            access_token = create_access_token(
+                user_data={
+                    "email": user.email,
+                    "user_uid": str(user.uid),
+                    "role": user.role,
+                }
             )
 
-            return JSONResponse(content={
-                "message": "Login successful",
-                "access_token": access_token, 
-                "refresh_token": referesh_token,
-                "user": {
-                    "uid": str(user.uid),
-                    "email": user.email,
-                    "first_name": user.first_name,
-                    "last_name": user.last_name
+            refresh_token = create_access_token(
+                user_data={"email": user.email, "user_uid": str(user.uid)},
+                refresh=True,
+                expiry=timedelta(days=REFRESH_TOKEN_EXPIRY),
+            )
+
+            return JSONResponse(
+                content={
+                    "message": "Login successful",
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "user": {"email": user.email, "uid": str(user.uid)},
                 }
-            }
-        )
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid email or password")
-            
-        
+            )
+
+    raise InvalidCredentials()
+
+
 @auth_router.get("/refresh_token")
 async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer())):
     expiry_timestamp = token_details["exp"]
@@ -61,16 +176,89 @@ async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer(
         new_access_token = create_access_token(user_data=token_details["user"])
 
         return JSONResponse(content={"access_token": new_access_token})
-    # return {"message": "New access token generated"}
-    # raise InvalidToken
+
+    raise InvalidToken
 
 
-@auth_router.post("/logout")
+@auth_router.get("/me", response_model=UserBooksModel)
+async def get_current_user(
+    user=Depends(get_current_user), _: bool = Depends(role_checker)
+):
+    return user
+
+
+@auth_router.get("/logout")
 async def revoke_token(token_details: dict = Depends(AccessTokenBearer())):
     jti = token_details["jti"]
 
     await add_jti_to_blocklist(jti)
 
-    return JSONResponse(content={"message": "Logout successfully"},
-                        status_code=status.HTTP_200_OK
+    return JSONResponse(
+        content={"message": "Logged Out Successfully"}, status_code=status.HTTP_200_OK
+    )
+
+
+
+@auth_router.post("/password-reset-request")
+async def password_reset_request(email_data: PasswordResetRequestModel):
+    email = email_data.email
+
+    token = create_url_safe_token({"email": email})
+    print(token)
+    link = f"http://{Config.DOMAIN}/api/v1/auth/password-reset-confirm/{token}"
+
+    html = f"""
+    <h1>Reset Your Password</h1>
+    <p>Please click this <a href="{link}">link</a> to Reset Your Password</p>
+    """
+    subject = "Reset Your Password"
+
+    emails = [email]
+    
+    # await mail.send_message(create_message(emails, subject, html))
+    send_email.delay(emails, subject, html)
+
+    return JSONResponse(
+        content={
+            "message": "Please check your email for instructions to reset your password",
+        },
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@auth_router.post("/password-reset-confirm/{token}")
+async def reset_account_password(
+    token: str,
+    passwords: PasswordResetConfirmModel,
+    session: AsyncSession = Depends(get_session),
+):
+    new_password = passwords.new_password
+    confirm_password = passwords.confirm_new_password
+
+    if new_password != confirm_password:
+        raise HTTPException(
+            detail="Passwords do not match", status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    token_data = decode_url_safe_token(token)
+
+    user_email = token_data.get("email")
+
+    if user_email:
+        user = await user_service.get_user_by_email(user_email, session)
+
+        if not user:
+            raise UserNotFound()
+
+        passwd_hash = generate_passwd_hash(new_password)
+        await user_service.update_user(user, {"password_hash": passwd_hash}, session)
+
+        return JSONResponse(
+            content={"message": "Password reset Successfully"},
+            status_code=status.HTTP_200_OK,
+        )
+
+    return JSONResponse(
+        content={"message": "Error occured during password reset."},
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
     )
