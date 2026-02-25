a42c9fb963184126b93f872c73cdb2a4656e76e9
dipanshu180 <dipanshu180503@gmail.com>
2026-02-25 23:23:56 +0530

    auth

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,6 +1,6 @@
 from typing import Any, List
 
-from fastapi import Depends, Request , HTTPException, status
+from fastapi import Depends, Request
 from fastapi.security import HTTPBearer
 from fastapi.security.http import HTTPAuthorizationCredentials
 from sqlmodel.ext.asyncio.session import AsyncSession
@@ -11,18 +11,18 @@ from src.db.redis import token_in_blocklist
 
 from .service import UserService
 from .utils import decode_token
-# from src.errors import (
-#     InvalidToken,
-#     RefreshTokenRequired,
-#     AccessTokenRequired,
-#     InsufficientPermission,
-#     AccountNotVerified,
-# )
+from src.errors import (
+    InvalidToken,
+    RefreshTokenRequired,
+    AccessTokenRequired,
+    InsufficientPermission,
+    AccountNotVerified,
+)
 
 user_service = UserService()
 
 
-class TokenBearer(HTTPBearer): 
+class TokenBearer(HTTPBearer):
     def __init__(self, auto_error=True):
         super().__init__(auto_error=auto_error)
 
@@ -33,12 +33,20 @@ class TokenBearer(HTTPBearer):
 
         token_data = decode_token(token)
 
+        if not self.token_valid(token):
+            raise InvalidToken()
+
         if await token_in_blocklist(token_data["jti"]):
-            raise HTTPException(status_code=401, detail="Token is blocked")
+            raise InvalidToken()
 
         self.verify_token_data(token_data)
 
         return token_data
+
+    def token_valid(self, token: str) -> bool:
+        token_data = decode_token(token)
+
+        return token_data is not None
 
     def verify_token_data(self, token_data):
         raise NotImplementedError("Please Override this method in child classes")
@@ -47,23 +55,24 @@ class TokenBearer(HTTPBearer):
 class AccessTokenBearer(TokenBearer):
     def verify_token_data(self, token_data: dict) -> None:
         if token_data and token_data["refresh"]:
-            raise HTTPException(status_code=401, detail="Access token required")
+            raise AccessTokenRequired()
+
 
 class RefreshTokenBearer(TokenBearer):
     def verify_token_data(self, token_data: dict) -> None:
         if token_data and not token_data["refresh"]:
-            raise HTTPException(status_code=401, detail="Refresh token required")
+            raise RefreshTokenRequired()
 
 
 async def get_current_user(
     token_details: dict = Depends(AccessTokenBearer()),
     session: AsyncSession = Depends(get_session),
-):
+):  
     user_email = token_details["user"]["email"]
 
     user = await user_service.get_user_by_email(user_email, session)
 
-    return user 
+    return user
 
 
 class RoleChecker:
@@ -72,8 +81,9 @@ class RoleChecker:
 
     def __call__(self, current_user: User = Depends(get_current_user)) -> Any:
         if not current_user.is_verified:
-            raise HTTPException(status_code=401, detail="Account not verified")
+            raise AccountNotVerified()
         if current_user.role in self.allowed_roles:
             return True
 
-        raise HTTPException(status_code=401, detail="Insufficient permissions")
+        raise InsufficientPermission()
+ 
