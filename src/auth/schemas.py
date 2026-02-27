2c25578e6f86fcf72e816bd71fcc4bd529a79bf4
dipanshu180 <dipanshu180503@gmail.com>
2026-02-27 23:25:12 +0530

    update

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,26 +1,63 @@
-from pydantic import BaseModel , Field
 import uuid
 from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel, Field
+
+from src.books.schemas import Book
+from src.reviews.schemas import ReviewModel
 
 
 class UserCreateModel(BaseModel):
-    first_name : str
-    last_name : str
-    username: str = Field(max_length=10)
-    email: str = Field(max_length=255)
-    password: str = Field(min_length=8)
+    first_name: str = Field(max_length=25)
+    last_name: str = Field(max_length=25)
+    username: str = Field(max_length=8)
+    email: str = Field(max_length=40)
+    password: str = Field(min_length=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "first_name": "John",
+                "last_name": "Doe",
+                "username": "johndoe",
+                "email": "johndoe123@co.com",
+                "password": "testpass123",
+            }
+        }
+    }
+
 
 class UserModel(BaseModel):
     uid: uuid.UUID
     username: str
     email: str
-    password_hash: str = Field(exclude=True)
-    first_name: str | None = None
-    last_name: str | None = None
+    first_name: str
+    last_name: str
     is_verified: bool
+    password_hash: str = Field(exclude=True)
     created_at: datetime
     update_at: datetime
 
+
+class UserBooksModel(UserModel):
+    books: List[Book]
+    reviews: List[ReviewModel]
+
+
 class UserLoginModel(BaseModel):
-    email: str = Field(max_length=255)
-    password: str = Field(min_length=8)
+    email: str = Field(max_length=40)
+    password: str = Field(min_length=6)
+
+
+class EmailModel(BaseModel):
+    addresses : List[str]
+
+
+class PasswordResetRequestModel(BaseModel):
+    email: str
+
+
+class PasswordResetConfirmModel(BaseModel):
+    new_password: str
+    confirm_new_password: str
