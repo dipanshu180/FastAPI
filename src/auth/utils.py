c58d7ac2a9447af6f9bb8589ff5e3df6fb839b2d
dipanshu180 <dipanshu180503@gmail.com>
2026-02-27 23:26:03 +0530

    update

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,64 +1,80 @@
-from passlib.context import CryptContext
-from fastapi import HTTPException, status
-from datetime import timedelta, datetime, timezone
-import jwt
-from src.config import Config
+import logging
 import uuid
-passwd_context = CryptContext(
+from datetime import datetime, timedelta
+from itsdangerous import URLSafeTimedSerializer
+
+import jwt
+from passlib.context import CryptContext
+
+from src.config import Config
+
+passwd_context =  CryptContext(
     schemes=["argon2"],
     deprecated="auto"
 )
 
-ACCESS_TOKEN_EXPIRE = 3600
 
-def generate_password_hash(password: str) -> str:
-    hashed_password = passwd_context.hash(password)
-    return hashed_password
+ACCESS_TOKEN_EXPIRY = 3600
 
-def verify_password(password: str, hashed_password: str) -> bool:
-    return passwd_context.verify(password, hashed_password)
+
+def generate_passwd_hash(password: str) -> str:
+    hash = passwd_context.hash(password)
+
+    return hash
+
+
+def verify_password(password: str, hash: str) -> bool:
+    return passwd_context.verify(password, hash)
+
 
 def create_access_token(
-    user_data: dict,
-    expiry: timedelta | None = None,
-    refresh: bool = False
+    user_data: dict, expiry: timedelta = None, refresh: bool = False
 ):
-    payload = user_data.copy()
-
-    now = datetime.now(timezone.utc)
+    payload = {}
 
     payload["user"] = user_data
-    payload["iat"] = now
-    payload["exp"] = now + (expiry if expiry else timedelta(seconds=ACCESS_TOKEN_EXPIRE))
+    payload["exp"] = datetime.now() + (
+        expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY)
+    )
     payload["jti"] = str(uuid.uuid4())
+
     payload["refresh"] = refresh
 
     token = jwt.encode(
-        payload=payload,
-        key=Config.JWT_SECRET,
-        algorithm=Config.JWT_ALGORITHM
+        payload=payload, key=Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHM
     )
 
     return token
 
+
 def decode_token(token: str) -> dict:
     try:
         token_data = jwt.decode(
-            jwt=token,
-            key=Config.JWT_SECRET,
-            algorithms=[Config.JWT_ALGORITHM]
+            jwt=token, key=Config.JWT_SECRET, algorithms=[Config.JWT_ALGORITHM]
         )
-        print(f"Decoded token data: {token_data}")
+       
         return token_data
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token has expired",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    except jwt.InvalidTokenError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or expired token",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+
+    except jwt.PyJWTError as e:
+        logging.exception(e)
+        return None
+
+serializer = URLSafeTimedSerializer(
+    secret_key=Config.JWT_SECRET, salt="email-configuration"
+)
+
+def create_url_safe_token(data: dict):
+
+    token = serializer.dumps(data)
+
+    return token
+
+def decode_url_safe_token(token:str):
+    try:
+        token_data = serializer.loads(token)
+
+        return token_data
+    
+    except Exception as e:
+        logging.error(str(e))
+        
